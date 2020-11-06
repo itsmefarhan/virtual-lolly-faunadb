bdffcab90c73a2d78e09303cbe71a20fe1e745ce
Farhan Farooq <farhan.farooq@live.com>
2020-11-06 11:34:57 +0500

    get lolly by link

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,24 @@
-// const path = require("path")
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
+const path = require("path")
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
 
-//   const { data } = await graphql(`
-//     query MyQuery {
-//       GetLollies {
-//         getLollies {
-//           link
-//         }
-//       }
-//     }
-//   `)
+  const { data } = await graphql(`
+    query MyQuery {
+      GetLollies {
+        getLollies {
+          link
+        }
+      }
+    }
+  `)
 
-//   data.GetLollies.getLollies.forEach(({ link }) => {
-//     createPage({
-//       path: `lolly/${link}`,
-//       component: path.resolve("./src/templates/lollytemplate.tsx"),
-//       context: {
-//         link: link,
-//       },
-//     })
-//   })
-// }
+  data.GetLollies.getLollies.forEach(({ link }) => {
+    createPage({
+      path: `lolly/${link}`,
+      component: path.resolve("./src/templates/lollytemplate.tsx"),
+      context: {
+        link: link,
+      },
+    })
+  })
+}
--- src/templates/lollytemplate.tsx
+++ src/templates/lollytemplate.tsx
@@ -1,61 +1,63 @@
-// import React from "react"
-// import Header from "../components/header"
-// import { Lolly } from "../components/Lolly"
-// import Result from "../components/result"
-// import { graphql } from "gatsby"
+import React from "react"
+import Header from "../components/header"
+import { Lolly } from "../components/Lolly"
+import Result from "../components/result"
+import { graphql } from "gatsby"
 
-// // interface Props {
-// //   data: {
-// //     get_lollies: {
-// //       topFlavor: string
-// //       middleFlavor: string
-// //       bottomFlavor: string
-// //       to: string
-// //       from: string
-// //       message: string
-// //       link: string
-// //     }
-// //   }
-// // }
+export const query = graphql`
+  query MyQuery($link: String!) {
+    GetLollies {
+      getLolly(link: $link) {
+        topFlavor
+        middleFlavor
+        bottomFlavor
+        to
+        from
+        message
+        link
+      }
+    }
+  }
+`
 
-// export const query = graphql`
-//   query MyQuery($link: String!) {
-//     GetLollies {
-//       getLolly(link: $link) {
-//         topFlavor
-//         middleFlavor
-//         bottomFlavor
-//         to
-//         from
-//         message
-//         link
-//       }
-//     }
-//   }
-// `
+interface Props {
+  data: {
+    GetLollies: {
+      getLolly: {
+        topFlavor: string
+        middleFlavor: string
+        bottomFlavor: string
+        to: string
+        from: string
+        message: string
+        link: string
+      }
+    }
+  }
+}
 
-// const LollyTemplate = ({ data }: any) => {
-//   return (
-//     <div>
-//       <Header />
-//       <div className="lollyFormDiv">
-//         <div>
-//           <Lolly
-//             top={data.GetLollies.getLolly.topFlavor}
-//             middle={data.GetLollies.getLolly.middleFlavor}
-//             bottom={data.GetLollies.getLolly.bottomFlavor}
-//           />
-//         </div>
+const LollyTemplate = ({ data }: Props) => {
+  return (
+    <div>
+      <Header />
+      <div className="lollyFormDiv">
+        <div>
+          <Lolly
+            top={data.GetLollies.getLolly.topFlavor}
+            middle={data.GetLollies.getLolly.middleFlavor}
+            bottom={data.GetLollies.getLolly.bottomFlavor}
+          />
+        </div>
 
-//         <Result
-//           link={data.GetLollies.getLolly.link}
-//           to={data.GetLollies.getLolly.to}
-//           from={data.GetLollies.getLolly.from}
-//           message={data.GetLollies.getLolly.message}
-//         />
-//       </div>
-//     </div>
-//   )
-// }
+        <Result
+          link={data.GetLollies.getLolly.link}
+          to={data.GetLollies.getLolly.to}
+          from={data.GetLollies.getLolly.from}
+          message={data.GetLollies.getLolly.message}
+        />
+      </div>
+    </div>
+  )
+}
 
-// export default LollyTemplate
+export default LollyTemplate
