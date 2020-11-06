f0ab1d27b46646c17b2400475c567e64404086b1
Farhan Farooq <farhan.farooq@live.com>
2020-11-06 20:07:12 +0500

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,56 @@
 import React from "react"
+import { useQuery, gql } from "@apollo/client"
+import Result from "../components/result"
+import { Lolly } from "../components/Lolly"
 
-const NotFoundPage = () => (
-  <>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const GET_LOLLY_BY_LINK = gql`
+  query GET_LOLLY($link: String!) {
+    getLolly(link: $link) {
+      topFlavor
+      middleFlavor
+      bottomFlavor
+      to
+      from
+      message
+      link
+    }
+  }
+`
+
+const NotFoundPage = ({ location }: any) => {
+  const loc = location.pathname.slice(0, 7)
+  const pathLoc = location.pathname.slice(7)
+
+  const { loading, data } = useQuery(GET_LOLLY_BY_LINK, {
+    variables: { link: pathLoc },
+  })
+
+  return (
+    <>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : !!data && loc === "/lolly/" ? (
+        <div className="lollyFormDiv">
+          <div>
+            <Lolly
+              top={data.getLolly.topFlavor}
+              middle={data.getLolly.middleFlavor}
+              bottom={data.getLolly.bottomFlavor}
+            />
+          </div>
+
+          <Result
+            link={data.getLolly.link}
+            to={data.getLolly.to}
+            from={data.getLolly.from}
+            message={data.getLolly.message}
+          />
+        </div>
+      ) : (
+        <h1 style={{ color: "white" }}>404: Not Found</h1>
+      )}
+    </>
+  )
+}
 
 export default NotFoundPage
